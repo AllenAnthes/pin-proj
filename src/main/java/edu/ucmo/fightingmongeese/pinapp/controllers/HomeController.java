5bb7fc05ac149089b016cd0a8ce3a402cc9291ec
AllenAnthes <abanthes@gmail.com>
2018-01-15 18:57:56 -0600

    fixed login view

--- src/main/java/edu/ucmo/fightingmongeese/pinapp/controllers/HomeController.java
+++ src/main/java/edu/ucmo/fightingmongeese/pinapp/controllers/HomeController.java
@@ -14,16 +14,16 @@ public class HomeController {
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login";
     }
 
     @GetMapping("/admin")
     public String admin() {
-        return "/admin";
+        return "admin";
     }
 
     @GetMapping("/403")
     public String error403() {
-        return "/403";
+        return "403";
     }
 }
